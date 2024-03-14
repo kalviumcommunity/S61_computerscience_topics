d401ece4a205cf0301ad363b2dc9552ba04ba0fe
Habeeb <skhabeebhussain91@.com>
2024-03-14 14:28:14 +0530

    Connected mongoDB

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 const app = express()
 const port = 3000
 
@@ -6,6 +8,18 @@ app.get('/ping', (req, res) => {
     res.send("This is an express app with ping route")
 })
   
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log("MongoDB connection error: ", err))
+
+
+app.get('/', (req, res) => {
+    if(mongoose.connection.readyState === 1){
+        res.send("Connected to MongoDB")
+    }else{
+        res.send("Did not connect to MongoDB")
+    }
+})
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
