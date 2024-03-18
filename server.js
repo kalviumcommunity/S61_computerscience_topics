b74c3f3cba0dfe41a1c1a45e652d85e692353526
Habeeb <skhabeebhussain91@.com>
2024-03-18 15:28:55 +0530

    Performed CRUD operations

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const router = require('./routes');
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(bodyParser.json());
 
 app.get('/ping', (req, res) => {
-    res.send("This is an express app with ping route")
-})
-  
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log("MongoDB connection error: ", err))
+    res.send("This is an express app with ping route");
+});
 
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log("MongoDB connection error: ", err));
 
 app.get('/', (req, res) => {
-    if(mongoose.connection.readyState === 1){
-        res.send("Connected to MongoDB")
-    }else{
-        res.send("Did not connect to MongoDB")
+    if (mongoose.connection.readyState === 1) {
+        res.send("Connected to MongoDB");
+    } else {
+        res.send("Did not connect to MongoDB");
     }
-})
+});
+
+app.use('/api', router);
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+    console.log(`App listening on port ${port}`);
+});
